588135c6900cc889a8932c3efeb186670a9a1312
Phani Katakam <phaniknv2001@gmail.com>
2021-10-29 14:22:52 +0530

    initial

---
+++ src/main/java/com/klef/demo/CddProject2Application.java
@@ -0,0 +1,13 @@
+package com.klef.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CddProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CddProject2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/klef/demo/ClientController.java
@@ -0,0 +1,12 @@
+package com.klef.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClientController {
+	@RequestMapping("/index")
+	public String display() {
+		return "index.html";
+	}
+}
